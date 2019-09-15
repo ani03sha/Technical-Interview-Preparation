2df1d14e041cd43d0aa1198923b9da94f265e405
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-09-15 19:08:38 +0530

    Updated the code of HandleTextComponentServlet

--- code/AEM/Servlets/SlingServlets/core/src/main/java/org/redquark/aem/servlets/examples/HandleTextComponentServlet.java
+++ code/AEM/Servlets/SlingServlets/core/src/main/java/org/redquark/aem/servlets/examples/HandleTextComponentServlet.java
@@ -2,6 +2,8 @@ package org.redquark.aem.servlets.examples;
 
 import java.io.IOException;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.servlet.Servlet;
 
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -14,10 +16,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * This servlet is registered via resource type of a component
+ * 
  * @author Anirudh Sharma
  */
-@Component(service = Servlet.class, property = { "sling.servlet.resourceTypes=slingservlets/components/content/text",
-		"sling.servlet.extensions=html", "sling.servlet.methods=" + HttpConstants.METHOD_GET })
+@Component(
+		service = Servlet.class, 
+		property = { 
+				"sling.servlet.resourceTypes=slingservlets/components/content/text",
+				"sling.servlet.extensions=html",
+				"sling.servlet.methods=" + HttpConstants.METHOD_GET 
+				}
+		)
 public class HandleTextComponentServlet extends SlingSafeMethodsServlet {
 
 	private static final long serialVersionUID = -4017298222572257376L;
@@ -25,14 +35,30 @@ public class HandleTextComponentServlet extends SlingSafeMethodsServlet {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Override
-	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
 
-		// Getting the resource object
-		Resource currentResource = request.getResource();
+		try {
+			// Getting the resource object
+			Resource currentResource = request.getResource();
 
-		log.info("Current resource path: {}", currentResource.getPath());
+			// Getting the reference of the corresponding node
+			Node currentNode = currentResource.adaptTo(Node.class);
 
-		response.getWriter().println("Current resource path: " + currentResource.getPath());
+			// Check if the node has property "text"
+			if (currentNode.hasProperty("text")) {
+
+				// Getting the value that is stored in "text" property
+				String text = currentNode.getProperty("text").getString();
+
+				log.info("Current resource path: {}", currentResource.getPath());
+
+				// Writing the value to the browser
+				response.getWriter().println("Text: " + text);
+			}
+
+		} catch (RepositoryException | IOException e) {
+			log.error(e.getMessage(), e);
+		}
 	}
 
 }
