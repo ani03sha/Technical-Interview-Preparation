6fb35e274bf38569b4e78a2a4ae6e1d7981cdccc
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-11-08 23:37:45 +0530

    Added the code for deleting a given node from a linked list

---
+++ code/Java/CompanyWise/Microsoft/src/test/java/org/redquark/techinterviews/microsoft/AppTest.java
@@ -0,0 +1,20 @@
+package org.redquark.techinterviews.microsoft;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
