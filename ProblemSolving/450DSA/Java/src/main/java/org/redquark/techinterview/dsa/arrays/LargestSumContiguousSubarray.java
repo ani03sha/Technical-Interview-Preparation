ba17607e68fa8e107a71427f7034616d562cccb9
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-01-06 20:09:47 +0530

    Added Largest Sum Contiguous Subarray problem in Java

---
+++ ProblemSolving/450DSA/Java/src/main/java/org/redquark/techinterview/dsa/arrays/LargestSumContiguousSubarray.java
@@ -0,0 +1,34 @@
+package org.redquark.techinterview.dsa.arrays;
+
+/**
+ * @author Anirudh Sharma
+ * <p>
+ * Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+ */
+public class LargestSumContiguousSubarray {
+
+    /**
+     * @param a - input array
+     * @return maximum sum
+     */
+    private static int maxSumSubarray(int[] a) {
+        // Global maxima
+        int globalMax = a[0];
+        int localMax = a[0];
+        // Loop through the elements of the list
+        for (int i = 1; i < a.length; i++) {
+            localMax = Math.max(a[i], a[i] + localMax);
+            globalMax = Math.max(localMax, globalMax);
+        }
+        return globalMax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSumSubarray(new int[]{1, 2, 3, -2, 5}));
+        System.out.println(maxSumSubarray(new int[]{-1, -2, -3, -4}));
+        System.out.println(maxSumSubarray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(maxSumSubarray(new int[]{1}));
+        System.out.println(maxSumSubarray(new int[]{0}));
+        System.out.println(maxSumSubarray(new int[]{-1}));
+    }
+}
