4837b618b66c3b25a1498f14393b9c4aa683d289
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-02-19 19:48:39 +0530

    Added Longest Repeating Subsequence problem in Python

--- ProblemSolving/450DSA/Python/src/string/LongestRepeatingSubsequence.py
+++ ProblemSolving/450DSA/Python/src/string/LongestRepeatingSubsequence.py
@@ -14,7 +14,7 @@ def longestRepeatingSubsequence(s):
     # Length of the string
     n = len(s)
     # DP array
-    dp=[[0 for i in range(n + 1)] for j in range(n + 1)]
+    dp = [[0 for i in range(n + 1)] for j in range(n + 1)]
     # Fill the DP array
     for i in range(1, n + 1):
         for j in range(1, n + 1):
