a0501e25e31bdd0180c22d58b690b2fade867707
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-02-21 21:29:58 +0530

    Added Building A RESTful Web Service example in Spring

---
+++ Frameworks/Spring/rest-service/src/main/java/org/redquark/techinterview/spring/restservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.redquark.techinterview.spring.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
---
+++ Frameworks/Spring/rest-service/src/test/java/org/redquark/techinterview/spring/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redquark.techinterview.spring.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
