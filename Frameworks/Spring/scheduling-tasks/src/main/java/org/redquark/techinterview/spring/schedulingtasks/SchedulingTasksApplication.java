68aa5d1f9f1efb32db71f6c19c079e9a99eab302
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-02-22 16:14:41 +0530

    Added Scheduling Tasks application in Spring Boot

---
+++ Frameworks/Spring/scheduling-tasks/src/main/java/org/redquark/techinterview/spring/schedulingtasks/SchedulingTasksApplication.java
@@ -0,0 +1,15 @@
+package org.redquark.techinterview.spring.schedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SchedulingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasksApplication.class, args);
+	}
+
+}
---
+++ Frameworks/Spring/scheduling-tasks/src/main/java/org/redquark/techinterview/spring/schedulingtasks/components/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package org.redquark.techinterview.spring.schedulingtasks.components;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        LOGGER.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
