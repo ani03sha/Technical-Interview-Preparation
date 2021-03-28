a45c86de663c70701b49f52c45679517373c6e95
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-03-28 16:49:47 +0530

    Added Search A Word In 2D Matrix problem in Java

--- ProblemSolving/450DSA/Java/src/main/java/org/redquark/techinterview/dsa/string/SearchAWordIn2DMatrix.java
+++ ProblemSolving/450DSA/Java/src/main/java/org/redquark/techinterview/dsa/string/SearchAWordIn2DMatrix.java
@@ -1,5 +1,21 @@
 package org.redquark.techinterview.dsa.string;
 
+/**
+ * @author Anirudh Sharma
+ * <p>
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ * <p>
+ * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are
+ * horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ * <p>
+ * Constraints:
+ * <p>
+ * m == board.length
+ * n = board[i].length
+ * 1 <= m, n <= 6
+ * 1 <= word.length <= 15
+ * board and word consists of only lowercase and uppercase English letters.
+ */
 public class SearchAWordIn2DMatrix {
 
     private static boolean search(char[][] board, String word) {
