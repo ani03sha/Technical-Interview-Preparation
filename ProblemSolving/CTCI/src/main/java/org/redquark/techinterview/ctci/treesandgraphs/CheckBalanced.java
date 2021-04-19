40cd1a958ed910dab73401bd09c1fd8e856693fc
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-04-19 19:04:22 +0530

    Added 4.4

--- ProblemSolving/CTCI/src/main/java/org/redquark/techinterview/ctci/treesandgraphs/CheckBalanced.java
+++ ProblemSolving/CTCI/src/main/java/org/redquark/techinterview/ctci/treesandgraphs/CheckBalanced.java
@@ -30,4 +30,22 @@ public class CheckBalanced {
         // Get the height of the the subtree rooted at node
         return 1 + Math.max(getHeight(node.left), getHeight(node.right));
     }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(isBalanced(root));
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.left.left.left = new TreeNode(4);
+        root.left.left.right = new TreeNode(4);
+        System.out.println(isBalanced(root));
+    }
 }
