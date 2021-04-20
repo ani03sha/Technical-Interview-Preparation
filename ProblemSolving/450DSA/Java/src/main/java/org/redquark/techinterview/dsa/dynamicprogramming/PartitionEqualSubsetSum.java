87f92a2116b02df925cdca1f79741213790337c9
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-04-20 18:57:01 +0530

    Added Partition Equal Subset Sum problem in Java

--- ProblemSolving/450DSA/Java/src/main/java/org/redquark/techinterview/dsa/dynamicprogramming/PartitionEqualSubsetSum.java
+++ ProblemSolving/450DSA/Java/src/main/java/org/redquark/techinterview/dsa/dynamicprogramming/PartitionEqualSubsetSum.java
@@ -31,7 +31,7 @@ public class PartitionEqualSubsetSum {
         Boolean[][] lookup = new Boolean[nums.length + 1][sum + 1];
         // If the sum is even, then we will fina a subarray
         // whose sum is equal to sum / 2
-        return isSubsetSum(nums, nums.length, sum / 2, lookup);
+        return isSubsetSum(nums, nums.length - 1, sum / 2, lookup);
     }
 
     private static boolean isSubsetSum(int[] nums, int n, int sum, Boolean[][] lookup) {
@@ -40,14 +40,14 @@ public class PartitionEqualSubsetSum {
             return true;
         }
         // Base case
-        if (sum < 0 || n <= 0) {
+        if (sum < 0 || n < 0) {
             return false;
         }
         // Check in cache
         if (lookup[n][sum] != null) {
             return lookup[n][sum];
         }
-        return lookup[n][sum] = isSubsetSum(nums, n - 1, sum - nums[n - 1], lookup)
+        return lookup[n][sum] = isSubsetSum(nums, n - 1, sum - nums[n], lookup)
                 || isSubsetSum(nums, n - 1, sum, lookup);
     }
 
