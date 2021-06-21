3bdcdaa159c27a55f683db5810cf3200dabe03ac
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-06-21 19:27:49 +0530

    Added Weighted Job Scheduling problem in JavaScript

--- ProblemSolving/450DSA/JavaScript/src/dynamicprogramming/WeightedJobScheduling.js
+++ ProblemSolving/450DSA/JavaScript/src/dynamicprogramming/WeightedJobScheduling.js
@@ -4,7 +4,7 @@
  * Given N jobs where every job is represented by following three elements of it.
  * 
  * Start Time
- * Finish Time
+ * Finish Time 
  * Profit or Value Associated (>= 0)
  * Find the maximum profit subset of jobs such that no two jobs in the subset overlap.
  */
