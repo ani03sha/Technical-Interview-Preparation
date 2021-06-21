2252e0dd3b269c5680fc0f6a0ea6e6675c713137
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-06-21 19:27:31 +0530

    Added Weighted Job Scheduling problem in Python

--- ProblemSolving/450DSA/JavaScript/src/dynamicprogramming/WeightedJobScheduling.js
+++ ProblemSolving/450DSA/JavaScript/src/dynamicprogramming/WeightedJobScheduling.js
@@ -1,3 +1,13 @@
+/**
+ * @author Anirudh Sharma
+ * 
+ * Given N jobs where every job is represented by following three elements of it.
+ * 
+ * Start Time
+ * Finish Time
+ * Profit or Value Associated (>= 0)
+ * Find the maximum profit subset of jobs such that no two jobs in the subset overlap.
+ */
 const maxProfitInJobs = (jobs) => {
     // Length of the array
     const n = jobs.length;
